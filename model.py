fb123bf55338399f34844014b89383a1e0469efd
kishore280517 <kishore1440@gmail.com>
2022-02-03 11:56:09 +0530

    added 3 models

---
+++ model.py
@@ -0,0 +1,10 @@
+print("branch creation")
+
+
+
+def model1():
+    print("adding code for ml model1")
+def model2():
+    print("adding code for ml model2")
+def model3():
+    print("adding code for ml model3")
